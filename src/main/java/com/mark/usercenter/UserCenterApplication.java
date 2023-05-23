23cd629e5d628d566b322ccf31ef7acf734dba14
mark_huangb <mark_huangb@163.com>
2023-05-23 11:52:15 +0800

    项目初始化

---
+++ src/main/java/com/mark/usercenter/UserCenterApplication.java
@@ -0,0 +1,15 @@
+package com.mark.usercenter;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.mark.usercenter.mapper")
+public class UserCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserCenterApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/mark/usercenter/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.mark.usercenter.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mark.usercenter.model.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
